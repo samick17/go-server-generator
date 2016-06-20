b5ba54098cced26d047d8f353e750a2f13283729
samick17 <boneache@gmail.com>
2016-06-20 18:34:31 +0800

    init

---
+++ generator.js
@@ -0,0 +1,5 @@
+module.exports = {
+  generate: function(config) {
+    return config;
+  }
+}
---
+++ test/generator.mocha.js
@@ -0,0 +1,19 @@
+const Generator = require('../generator');
+
+var assert = require('assert');
+
+describe('Supposed we have a generator', function() {
+
+  beforeEach(function(done) {
+    done();
+  });
+
+  afterEach(function(done) {
+    done();
+  });
+
+  it('generate server with empty config', function(done) {
+    Generator.generate('').should.equal('');
+    done();
+  });
+});
